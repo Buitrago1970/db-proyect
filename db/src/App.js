3066cecdae8bcfb0bd77d3932319aae814776c5d
Juan Buitrago <juanbuitrago2002@outlook.com>
2021-03-23 21:52:29 -0500

    creando proyecto

---
+++ db/src/App.js
@@ -0,0 +1,23 @@
+import logo from "";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>pagina para conocer elamor de sandoval</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          APRENDE MAS
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
